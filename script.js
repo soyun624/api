2302c14180852df268ee7224880c24e595ed3fed
Soyun <soyunlee624@gmail.com>
2020-10-21 10:20:52 -0700

    added favourite books in local storage

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(function(){
+    let favouriteBooks = localStorage.getItem('favourite') ? JSON.parse(localStorage.getItem('favourite')) : [];
+
+    console.log(favouriteBooks)
     $.ajax({
         url: "https://api.nytimes.com/svc/books/v3/lists/current/childrens-middle-grade-hardcover.json?api-key=grMuMJr9zYeKe98uSf67PQsougSfcpJp", 
         dataType: 'json',
@@ -6,11 +9,25 @@ $(function(){
             let name = res.results.list_name;
             let date = res.results.published_date;
             let bookData = dataSanitizer(res);
-            
+ 
+            const checkFavourite = (item) => {
+
+                const index = favouriteBooks.findIndex(e => e.id === item.id);
+                return (index < 0 ) ? false : true;
+               
+            }
+
             bookData.forEach(data => {
-                const itemDiv = $('<div class="book-item"></div>');
-                const rank = $('<p></p>').text(`Rank : ${data.rank}`);
+                // const isFavourite = checkFavourite(data);
+                const itemDiv = $('<div class="book-item"></div>').attr('id',data.primary_isbn10);
+                const rank = $('<p></p>').text(`${data.rank}`);
                 itemDiv.append(rank);
+                
+                // const icon = isFavourite ? $('<i class="fas fa-heart favourite"></i>') : $('<i class="fas fa-heart"></i>');
+             
+                const icon = $('<i class="fas fa-heart"></i>');
+ 
+                itemDiv.append(icon);
                 const title = $('<h4></h4>').text(data.title);
                 itemDiv.append(title);
                 const primary_isbn10 = $('<p></p>').text(`Isbn : ${data.primary_isbn10}`);
@@ -32,11 +49,37 @@ $(function(){
                 $('.list').append(itemDiv);
 
             })
-       
+
+            //   let favouriteBooks = [];
+            
+            $('.fa-heart').click(function(e){
+        
+                let bookData = [];
+                let target = $(e.target);
+                let targetId = target.parent().attr('id')
+                let isFav = target.hasClass('favourite')
+
+                const favBook = bookData.find(e => e.id === targetId);
+                const favIndex = favouriteBooks.findIndex(e => e.id === targetId);
+
+                console.log("e.id : "+e.id);
+                console.log("targetId : "+targetId);
+
+                if (!isFav) {
+                    favouriteBooks.push(favBook);
+                } else {
+                    favouriteBooks.splice(favIndex, 1)
+                }               
+           localStorage.setItem('favourite', JSON.stringify(favouriteBooks));
+                target.toggleClass('favourite');
+                console.log(favouriteBooks);
+            
+            })
    
         }
-        
+    
     })
+
 })
 
 function dataSanitizer(response){
